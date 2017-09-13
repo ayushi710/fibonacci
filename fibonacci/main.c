13592d1f4c8019afb460d64392e132a391d51222
ayushi <ayushimittal710@gmail.com>
2017-09-13 19:34:18 +0530

    Initial Commit

---
+++ fibonacci/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  fibonacci
+//
+//  Created by AYUSHI MITTAL on 13/09/17.
+//  Copyright © 2017 AYUSHI MITTAL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
